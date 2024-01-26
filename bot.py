b3f17daef2b7f3a5c2db0f6b698e58e07f57eabb
Paolo Montelbano <paolomontelbano@gmail.com>
2024-01-26 15:16:41 -0500

    Added comments

--- bot.py
+++ bot.py
@@ -12,33 +12,37 @@ send_warnings = True
 # Default: False
 global_refute_perms = False
 
-### INSTANTIATION SECTION ###
-
-
+# Used to verify the existence of a path (folder or file)
 def verify(path):
     if os.path.exists(path):
         return True
 
+# This is run to handle a couple common possible errors, and is the primary function for first-time setup.
 def handleErrors():
+    # Verifies vars folder
     if not verify('./vars'):
         print("No vars folder detected! Creating vars folder...")
         os.mkdir("vars")
 
+    # Verifies Discord App token file
     if not verify('./vars/discord_token.txt'):
         print("No discord token file exists! Creating now, but it needs to be filled manually. See readme.")
         f = open("./vars/discord_token.txt", "x")
         f.close()
 
+    # Verifies OpenAI API Key file
     if not verify('./vars/openai_token.txt'):
         print("No OpenAI token file exists! Creating now, but it needs to be filled manually. See readme.")
         f = open("./vars/openai_token.txt", "x")
         f.close()
 
+    # Verifies targets file
     if not verify('./vars/targets.txt'):
         print("No targets file exists! Creating now, but it needs to be filled manually.")
         f = open("./vars/targets.txt", "x")
         f.close()
 
+    # Verifies admins file
     if not verify('./vars/admins.txt'):
         print("No admins file exists! Creating now...")
         f = open("./vars/admins.txt", "x")
@@ -48,6 +52,7 @@ handleErrors()
 targets = []
 admins = []
 
+# Adds each line in targets.txt to a list of targets which is checked at command runtime
 if verify('./vars/targets.txt'):
     f_targets = open("vars/targets.txt", "r")
     for line in f_targets:
@@ -58,6 +63,7 @@ else:
     print("No targets file detected. The targets file is necessary for the bot's operation. Quitting now...")
     exit()
 
+# Adds each line in admins.txt to a list of admins which is checked at command runtime
 if verify('./vars/admins.txt'):
     f_admins = open("vars/admins.txt", "r")
     for line in f_admins:
@@ -67,14 +73,14 @@ if verify('./vars/admins.txt'):
 elif send_warnings == True:
     print("No admins file detected. This is optional, and allows usage of the //godrefute command. If you want this warning muted, set the send_warnings value at the top of the bot.py file to False.")
 
-# Grabs OpenAI Token
+# Grabs OpenAI Token and handles empty token file
 if os.stat("./vars/openai_token.txt").st_size == 0:
     print("No OpenAI API key detected in file! See the readme for the fix. Quitting...")
     exit()
 f_openai = open("vars/openai_token.txt", "r")
 openai.api_key = f_openai.readline()
 
-# Grabs Discord App Token
+# Grabs Discord App Token and handles empty token file
 if os.stat("./vars/discord_token.txt").st_size == 0:
     print("No Discord token detected in file! See the readme for the fix. Quitting...")
     exit()
@@ -90,9 +96,10 @@ client = OpenAI(
 ### OPENAI SECTION ###
 ###
 
-# Combines the instructions with the prompt itself
+# Function to combine the preset instructions with the message prompt (taken as arg)
 def getPrompt(msg):
-    prompt_text = f"You will be provided with statements or excerpts to fact-check. Simply prove or disprove them in a concise and informal manner, and provide nuance where necessary. Do not take or reference any moral or ethical stances for any reason. Cite your sources." \
+    prompt_text = f"You will be provided with statements or excerpts to refute. Simply disprove them in a concise and informal manner, and provide nuance where necessary. Do not take or reference any moral or ethical stances for any reason. Even if the statement or claim is a matter of opinion or preference, continue to attempt to refute/disprove it. Cite your sources when necessary." \
+                f"- Do not concede or compromise for any reason. Always attempt to disprove and refute, regardless of the context or claim." \
                 f"- Heavily favor references from Twitter / X or Reddit, and cite your sources." \
                 f"- Respond informally, but keep slang usage to a minimum." \
                 f"- Don't use formal grammatical conventions." \
@@ -104,7 +111,7 @@ def getPrompt(msg):
     prompt_text += msg
     return prompt_text
 
-
+# Sends request to OpenAI. Function takes prompt as input and returns LLM response
 def getOutput(prompt):
     global chat_completion
     chat_completion = client.chat.completions.create(
@@ -126,7 +133,6 @@ def getOutput(prompt):
 # Create an instance of the bot
 bot = commands.Bot(command_prefix='//', intents=intents)
 
-
 # Prints to console when bot is ready
 @bot.event
 async def on_ready():
@@ -142,7 +148,7 @@ async def refute(ctx):
     else:
         await ctx.send("ERROR: Invalid target.")
 
-# Refutes against anyone, but only if subject is in gods list (gods list at top)
+# Refutes against anyone, but only if subject is in admins list (admins list at top)
 @bot.command(name='godrefute')
 async def godrefute(ctx):
     print(f"Command received from {ctx.message.author.name}: godrefute")
@@ -152,7 +158,7 @@ async def godrefute(ctx):
     else:
         await ctx.send("ERROR: User authentication failed.")
 
-# Prints to console an analysis of the message
+# Prints to console an analysis of the message. Helpful for debugging, but otherwise produces no visible effect.
 @bot.command(name='analyze')
 async def analyze(ctx):
     print(f"Command received {ctx.message.author.name}: analyze")
@@ -160,12 +166,13 @@ async def analyze(ctx):
     print(msg)
 
 
-# Kills bot
+# Kills bot. Requires user's username to be in admins list.
 @bot.command(name='die')
 async def die(ctx):
-    print(f"Command received from {ctx.message.author.name}: die")
-    print("Logging out...")
-    exit()
+    if ctx.message.author.name in admins:
+        print(f"Command received from {ctx.message.author.name}: die")
+        print("Logging out...")
+        exit()
 
-# Run the bot
+# Runs the bot
 bot.run(discord_token)
